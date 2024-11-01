eeeef2dec5be0935536c94d9e54e3ee669318175
Oussamahamani <oussamahamani2000@gmail.com>
2024-11-01 13:14:31 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const cities = [
+    { city: "Boston", temperature: 71 },
+    { city: "New York", temperature: 75 },
+    { city: "New York", temperature: 75 },
+    { city: "Los Angeles", temperature: 80 },
+    { city: "Chicago", temperature: 65 },
+    { city: "Houston", temperature: 90 },
+    { city: "Denver", temperature: 45 }
+];
+
+
+
+
+
+
